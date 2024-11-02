b982290f979459eefa2b10067ad06f3e77f02a33
Sam Gardner <samuel.gardner@ttu.edu>
2024-11-01 23:49:05 -0500

    rastered satellite

--- mpl_generate.py
+++ mpl_generate.py
@@ -3,6 +3,11 @@ from datetime import datetime as dt, timedelta
 from pathlib import Path
 
 
+from goes2go import GOES
+from pyxlma import coords
+from glmtools.io.lightning_ellipse import lightning_ellipse_rev
+
+
 from dask.distributed import Client, wait, print
 import numpy as np
 import xarray as xr
@@ -13,6 +18,63 @@ from cartopy import crs as ccrs
 from cartopy import feature as cfeat
 import cmweather
 from metpy.plots import USCOUNTIES
+
+
+def plot_satellite(path_to_read, this_time, min_x, max_x, min_y, max_y, channel_select):
+    print('Plotting satellite')
+    mpl_use('agg')
+    sat = xr.open_dataset(path_to_read)
+    padding = .001
+    area_i_want = sat.sel(y=slice(max_y+padding, min_y-padding), x=slice(min_x-padding, max_x+padding))
+    geosys = coords.GeographicSystem()
+    ltg_ell = lightning_ellipse_rev[1]
+    satsys = coords.GeostationaryFixedGridSystem(subsat_lon=sat.nominal_satellite_subpoint_lon.data.item(), sweep_axis='x', ellipse=ltg_ell)
+    this_satellite_scan_x, this_satellite_scan_y = np.meshgrid(area_i_want.x, area_i_want.y)
+    sat_ecef_X, sat_ecef_Y, sat_ecef_Z = satsys.toECEF(this_satellite_scan_x.flatten(), this_satellite_scan_y.flatten(), np.zeros_like(this_satellite_scan_x.flatten()))
+    sat_lon, sat_lat, _ = geosys.fromECEF(sat_ecef_X, sat_ecef_Y, sat_ecef_Z)
+    sat_lon.shape = this_satellite_scan_x.shape
+    sat_lat.shape = this_satellite_scan_y.shape
+    if channel_select == 'CMI_C02':
+        cmap_to_use = 'Greys_r'
+    else:
+        cmap_to_use = 'viridis_r'
+    
+    fig = plt.figure()
+    ax = plt.axes(projection=ccrs.epsg(3857))
+    pcm = ax.pcolormesh(sat_lon, sat_lat, area_i_want[channel_select].data, cmap=cmap_to_use, transform=ccrs.PlateCarree())
+    ax.add_feature(USCOUNTIES.with_scale('5m'), edgecolor='gray', linewidth=0.5)
+    ax.add_feature(cfeat.COASTLINE.with_scale('10m'), edgecolor='gray', linewidth=0.5)
+    lim_mins = (-98.3, 25.5)
+    lim_maxs = (-91, 32)
+    ax.set_extent([lim_mins[0], lim_maxs[0], lim_mins[1], lim_maxs[1]], crs=ccrs.PlateCarree())
+    px = 1/plt.rcParams['figure.dpi']
+    fig.set_size_inches(2048*px, 2048*px)
+    extent = ax.get_tightbbox(fig.canvas.get_renderer()).transformed(fig.dpi_scale_trans.inverted())
+    path_to_save = path.join('/Volumes','LtgSSD','analysis', 'mpl-generated', 'sat', f'{channel_select.replace(" ", "")}_{this_time.strftime("%Y%m%d_%H%M%S")}.png')
+    Path(path.dirname(path_to_save)).mkdir(parents=True, exist_ok=True)
+    fig.savefig(path_to_save, bbox_inches=extent, transparent=True)
+    print(path_to_save)
+    plt.close(fig)
+
+
+def queue_satellite(time, dataset_time, min_x, max_x, min_y, max_y, client=None):
+    res = []
+    g16 = GOES(satellite=16, product='ABI-L2-MCMIPC')
+    dl_res = g16.timerange(dataset_time, dataset_time + timedelta(days=1), max_cpus=12)
+    dl_res['valid'] = dl_res[['start', 'end']].mean(axis=1)
+    dl_res['path'] = '/Volumes/LtgSSD/' + dl_res['file'].values
+    for this_time in time:
+        closest_time = dl_res.iloc[(dl_res['valid'] - this_time).abs().argsort()[:1]]
+        this_path = closest_time['path'].values[0]
+        for channel_to_plot in ['CMI_C02', 'CMI_C13']:
+            this_time = np.array([this_time]).astype('datetime64[s]').astype(dt)[0]
+            if client is None:
+                plot_satellite(this_path, this_time, min_x, max_x, min_y, max_y, channel_to_plot)
+            else:
+                t = client.submit(plot_satellite, this_path, this_time, min_x, max_x, min_y, max_y, channel_to_plot, pure=False)
+                res.append(t)
+    if client is not None:
+        return res
 
 
 def plot_radar(time, dataset_time, radar, var_to_plot):
@@ -75,7 +137,7 @@ def plot_radar(time, dataset_time, radar, var_to_plot):
     px = 1/plt.rcParams['figure.dpi']
     fig.set_size_inches(2048*px, 2048*px)
     extent = ax.get_tightbbox(fig.canvas.get_renderer()).transformed(fig.dpi_scale_trans.inverted())
-    path_to_save = path.join('/Volumes','LtgSSD','analysis', 'mpl-generated', radar, f'{var_to_plot}_{time.strftime("%Y%m%d_%H%M%S")}.png')
+    path_to_save = path.join('/Volumes','LtgSSD','analysis', 'mpl-generated', radar, f'{var_to_plot.replace(" ", "")}_{time.strftime("%Y%m%d_%H%M%S")}.png')
     Path(path.dirname(path_to_save)).mkdir(parents=True, exist_ok=True)
     fig.savefig(path_to_save, bbox_inches=extent, transparent=True)
     print(path_to_save)
@@ -84,7 +146,7 @@ def plot_radar(time, dataset_time, radar, var_to_plot):
 
 def queue_radar(times, date_i_want, client=None):
     times = times.astype('datetime64[s]').astype(dt)
-    radar = ['KHGX', 'TIAH', 'THOU'] # KLCH
+    radar = ['KHGX', 'TIAH', 'THOU', 'KLCH']
     all_vars = ['Reflectivity', 'Velocity', 'Spectrum Width']
     dualpol_vars = ['RhoHV', 'ZDR']
     res = []
@@ -104,7 +166,7 @@ def queue_radar(times, date_i_want, client=None):
                         t = client.submit(plot_radar, time, date_i_want, r, var, pure=False)
                         res.append(t)
     if client is not None:
-        return client.gather(res)
+        return res
 
 
 
@@ -114,4 +176,16 @@ if __name__ == '__main__':
     tfm = xr.open_dataset(f'/Volumes/LtgSSD/tobac_saves/tobac_Save_{date_i_want.strftime("%Y%m%d")}/Track_features_merges_augmented2.zarr',
                             engine='zarr', chunks='auto')
     times = tfm.time.data
-    queue_radar(times, date_i_want, client)
+    all_res = []
+
+    radar_res = queue_radar(times, date_i_want, client)
+    all_res.extend(radar_res)
+    sat_min_x = tfm.g16_scan_x.min().data.compute()
+    sat_max_x = tfm.g16_scan_x.max().data.compute()
+    sat_min_y = tfm.g16_scan_y.min().data.compute()
+    sat_max_y = tfm.g16_scan_y.max().data.compute()
+    sat_res = queue_satellite(times, date_i_want, sat_min_x, sat_max_x, sat_min_y, sat_max_y, client)
+
+    all_res.extend(sat_res)
+    print('GATHERING!')
+    client.gather(all_res)
--- my_polyline.py
+++ my_polyline.py
@@ -6,98 +6,52 @@ import geoviews as gv
 import panel as pn
 
 import geopandas as gpd
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, LineString
 
 import numpy as np
 import xarray as xr
-import pyart
 
 from goes2go import GOES
 
 from pyxlma import coords
 from glmtools.io.lightning_ellipse import lightning_ellipse_rev
-import cmweather
 
 from functools import partial
 
 
 import sys
 
+lim_mins = (-98.3, 25.5)
+lim_maxs = (-91, 32)
+xmin, ymin = hv.util.transform.lon_lat_to_easting_northing(*lim_mins)
+xmax, ymax = hv.util.transform.lon_lat_to_easting_northing(*lim_maxs)
+
 hv.extension('bokeh')
 
 select_geoms = {}
 
-def plot_satellite(dl_res, time, min_x, max_x, min_y, max_y, channel_select, satellite_tick):
+def plot_satellite(time, channel_select, satellite_tick):
+    bnds = (lim_mins[0], lim_mins[1]+.09, lim_maxs[0], lim_maxs[1])
+    blank = gv.RGB((np.array([0, 1]), np.array([0, 1]), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)))).opts(visible=False)
     if not satellite_tick:
-        return gv.QuadMesh(([0, 1], [0, 1], [[0, 1], [2, 3]]), kdims=['Longitude', 'Latitude'], vdims=[channel_select]).opts(visible=False)
-    print('Plotting satellite')
-    closest_time = dl_res.iloc[(dl_res['valid'] - time).abs().argsort()[:1]]
-    sat = xr.open_dataset(closest_time['path'].values[0])
-    padding = .001
-    area_i_want = sat.sel(y=slice(max_y+padding, min_y-padding), x=slice(min_x-padding, max_x+padding))
-    geosys = coords.GeographicSystem()
-    ltg_ell = lightning_ellipse_rev[1]
-    satsys = coords.GeostationaryFixedGridSystem(subsat_lon=sat.nominal_satellite_subpoint_lon.data.item(), sweep_axis='x', ellipse=ltg_ell)
-    this_satellite_scan_x, this_satellite_scan_y = np.meshgrid(area_i_want.x, area_i_want.y)
-    sat_ecef_X, sat_ecef_Y, sat_ecef_Z = satsys.toECEF(this_satellite_scan_x.flatten(), this_satellite_scan_y.flatten(), np.zeros_like(this_satellite_scan_x.flatten()))
-    sat_lon, sat_lat, _ = geosys.fromECEF(sat_ecef_X, sat_ecef_Y, sat_ecef_Z)
-    sat_lon.shape = this_satellite_scan_x.shape
-    sat_lat.shape = this_satellite_scan_y.shape
-    if channel_select == 'CMI_C02':
-        cmap_to_use = 'Greys'
-    else:
-        cmap_to_use = 'viridis'
-    plot = gv.QuadMesh((sat_lon, sat_lat, area_i_want.CMI_C13.data), kdims=['Longitude', 'Latitude'], vdims=[channel_select]).opts(cmap=cmap_to_use, visible=satellite_tick)
+        return blank
+    path_to_image = path.join('/Volumes','LtgSSD','analysis', 'mpl-generated', 'sat', f'{channel_select}_{time.strftime("%Y%m%d_%H%M%S")}.png')
+    if not path.exists(path_to_image):
+        return blank
+    plot = gv.RGB.load_image(path_to_image, bounds=bnds)
     print(f'Satellite plot done for {time}')
     return plot
 
 
-def plot_radar(dataset, time, radar_selector, radar_tick):
+def plot_radar(radar, time, radar_selector, radar_tick):
+    bnds = (lim_mins[0], lim_mins[1]+.09, lim_maxs[0], lim_maxs[1])
+    blank = gv.RGB((np.array([0, 1]), np.array([0, 1]), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)))).opts(visible=False)
     if not radar_tick:
-        return gv.QuadMesh(([0, 1], [0, 1], [[0, 1], [2, 3]]), kdims=['Longitude', 'Latitude'], vdims=[radar_selector]).opts(visible=False)
-    print('Plotting radar')
-    ds_times = [dt.strptime(path.basename(f)[4:-4], '%Y%m%d_%H%M%S') for f in dataset]
-    ds_timedeltas = [abs(time - t) for t in ds_times]
-    if min(ds_timedeltas) > timedelta(minutes=15):
-        return gv.QuadMesh(([0, 1], [0, 1], [[0, 1], [2, 3]]), kdims=['Longitude', 'Latitude'], vdims=[radar_selector]).opts(visible=False)
-    rdr = pyart.io.read(dataset[np.argmin(ds_timedeltas)])
-    nyq = rdr.instrument_parameters['nyquist_velocity']['data'].max()
-    if nyq == 0:
-        maxr = rdr.instrument_parameters['unambiguous_range']['data'].min()
-        prf = 2.998e8/(2*maxr)
-        nyq = .05*prf/4
-    if radar_selector == 'Reflectivity':
-        data2plot = 'reflectivity'
-        cmap_to_use = 'ChaseSpectral'
-        sweep_i_want = 0
-        clim_to_use = (-10, 80)
-    elif radar_selector == 'RhoHV':
-        data2plot = 'cross_correlation_ratio'
-        cmap_to_use = 'plasmidis'
-        sweep_i_want = 0
-        clim_to_use = (0, 1)
-    elif radar_selector == 'ZDR':
-        data2plot = 'differential_reflectivity'
-        cmap_to_use = 'turbone'
-        sweep_i_want = 0
-        clim_to_use = (-2, 8)
-    elif radar_selector == 'Velocity':
-        data2plot = 'velocity'
-        cmap_to_use = 'balance'
-        sweep_i_want = 1
-        clim_to_use = (-nyq/2, nyq/2)
-    elif radar_selector == 'Spectrum Width':
-        data2plot = 'spectrum_width'
-        cmap_to_use = 'cubehelix_r'
-        sweep_i_want = 1
-        clim_to_use = (0, nyq/2)
-    rdr = rdr.extract_sweeps([sweep_i_want])
-    lats, lons, _ = rdr.get_gate_lat_lon_alt(0)
-    data2plot = rdr.fields[data2plot]['data']
-    plot = gv.QuadMesh((lons, lats, data2plot), kdims=['Longitude', 'Latitude'], vdims=[radar_selector]).opts(
-        cmap=cmap_to_use, colorbar=False, tools=['hover'], visible=radar_tick)
-    if clim_to_use is not None:
-        plot = plot.opts(clim=clim_to_use)
+        return blank
+    path_to_image = path.join('/Volumes','LtgSSD','analysis', 'mpl-generated', radar, f'{radar_selector.replace(" ", "")}_{time.strftime("%Y%m%d_%H%M%S")}.png')
+    if not path.exists(path_to_image):
+        return blank
+    plot = gv.RGB.load_image(path_to_image, bounds=bnds)
     print(f'Radar plot done for {time}')
     return plot
 
@@ -151,6 +105,7 @@ def write_json(_):
 if __name__ == '__main__':
     date_i_want = dt(2022, 6, 2, 0, 0, 0)
     tfm = xr.open_dataset(f'/Volumes/LtgSSD/tobac_saves/tobac_Save_{date_i_want.strftime("%Y%m%d")}/Track_features_merges_augmented2.zarr', engine='zarr', chunks='auto')
+    unique_times = np.unique(tfm.time.data).astype('datetime64[us]').astype(dt).tolist()
 
     grid_max_lon = tfm.lon.max().compute()
     grid_min_lon = tfm.lon.min().compute()
@@ -159,13 +114,6 @@ if __name__ == '__main__':
 
     goes_time_range_start = tfm.time.data.astype('datetime64[us]').astype(dt).min()
     goes_time_range_end = tfm.time.data.astype('datetime64[us]').astype(dt).max()
-
-    radar_files = [f for f in sorted(listdir(f'/Volumes/LtgSSD/nexrad_l2/{date_i_want.strftime("%Y%m%d")}/')) if f.endswith('V06') or f.endswith('V08')]
-    radar_files = [f'/Volumes/LtgSSD/nexrad_l2/{date_i_want.strftime("%Y%m%d")}/' + f for f in radar_files]
-    khgx_files = [f for f in radar_files if 'KHGX' in f]
-    klch_files = [f for f in radar_files if 'KLCH' in f]
-    tiah_files = [f for f in radar_files if 'TIAH' in f]
-    thou_files = [f for f in radar_files if 'THOU' in f]
 
     lma = xr.open_dataset('/Volumes/LtgSSD/'+date_i_want.strftime('%B').lower()+date_i_want.strftime('/6sensor_minimum/LYLOUT_%y%m%d_000000_86400_map500m.nc'),
                           chunks='auto')
@@ -176,18 +124,10 @@ if __name__ == '__main__':
     sat_min_y = tfm.g16_scan_y.min().data.compute()
     sat_max_y = tfm.g16_scan_y.max().data.compute()
 
-    g16 = GOES(satellite=16, product='ABI-L2-MCMIPC')
-    download_results = g16.timerange(goes_time_range_start-timedelta(minutes=15), goes_time_range_end+timedelta(minutes=15), max_cpus=12)
-    download_results['valid'] = download_results[['start', 'end']].mean(axis=1)
-    download_results['path'] = '/Volumes/LtgSSD/' + download_results['file'].values
-    unique_times = np.unique(tfm.time.data).astype('datetime64[us]').astype(dt).tolist()
-
     date_slider = pn.widgets.Select(name='Date', options=unique_times, value=unique_times[0])
     channel_select = pn.widgets.Select(name='Satellite Channel', options=['CMI_C02', 'CMI_C13'], value='CMI_C02')
     satellite_tick = pn.widgets.Checkbox(name='Show Satellite')
-    satellite = hv.DynamicMap(pn.bind(plot_satellite, download_results, date_slider,
-                                      sat_min_x, sat_max_x, sat_min_y, sat_max_y,
-                                      channel_select, satellite_tick))
+    satellite = hv.DynamicMap(pn.bind(plot_satellite, date_slider, channel_select, satellite_tick))
     
     seg_tick = pn.widgets.Checkbox(name='Show Segmentation')
     seg_sel = pn.widgets.Select(name='Grid Product', options=['Feature ID', 'Cell ID'], value='Cell ID')
@@ -196,30 +136,27 @@ if __name__ == '__main__':
 
     radar_sel = pn.widgets.Select(name='Radar Product', options=['Reflectivity', 'RhoHV', 'ZDR', 'Velocity', 'Spectrum Width'], value='Reflectivity')
     hgx_tick = pn.widgets.Checkbox(name='Show KHGX', value=False)
-    radar_mesh_hgx = hv.DynamicMap(pn.bind(plot_radar, khgx_files, date_slider, radar_sel, hgx_tick))
+    radar_mesh_hgx = hv.DynamicMap(pn.bind(plot_radar, 'KHGX', date_slider, radar_sel, hgx_tick))
     lch_tick = pn.widgets.Checkbox(name='Show KLCH', value=False)
-    radar_mesh_lch = hv.DynamicMap(pn.bind(plot_radar, klch_files, date_slider, radar_sel, lch_tick))
+    radar_mesh_lch = hv.DynamicMap(pn.bind(plot_radar, 'KLCH', date_slider, radar_sel, lch_tick))
     iah_tick = pn.widgets.Checkbox(name='Show TIAH', value=False)
-    radar_mesh_iah = hv.DynamicMap(pn.bind(plot_radar, tiah_files, date_slider, radar_sel, iah_tick))
+    radar_mesh_iah = hv.DynamicMap(pn.bind(plot_radar, 'TIAH', date_slider, radar_sel, iah_tick))
     hou_tick = pn.widgets.Checkbox(name='Show THOU', value=False)
-    radar_mesh_hou = hv.DynamicMap(pn.bind(plot_radar, thou_files, date_slider, radar_sel, hou_tick))
+    radar_mesh_hou = hv.DynamicMap(pn.bind(plot_radar, 'THOU', date_slider, radar_sel, hou_tick))
 
-    polygon = gv.Polygons([[(-120, 26.5), (-50, 26.5), (-50, 20), (-120, 20)], [(-91.5, 26.5), (-50, 26.5), (-50, 70), (-91.5, 70)]]).opts(fill_alpha=0.3, fill_color='black')
+    polygon = gv.Polygons([]).opts(fill_alpha=0.3, fill_color='black')
     sfc_lasso = hv.streams.PolyDraw(source=polygon, drag=False, num_objects=1)
     handle_with_time = partial(handle_lasso, time=date_slider)
     sfc_lasso.add_subscriber(handle_with_time)
 
-    # lower_limit = gv.HLine(26.5).opts(color='red')
-    # eastern_limit = gv.VLine(-91.5).opts(color='red')
+    lower_limit_line = LineString([(-120, 26.5), (-91.5, 26.5)])
+    lower_limit = gv.Shape(lower_limit_line).opts(color='red')
+    eastern_limit_line = LineString([(-91.5, 26.5), (-91.5, 70)])
+    eastern_limit = gv.Shape(eastern_limit_line).opts(color='red')
 
     write_btn = pn.widgets.Button(name='Write JSON')
     pn.bind(write_json, write_btn, watch=True)
 
-
-    lim_mins = (-98.3, 25.5)
-    lim_maxs = (-91, 32)
-    xmin, ymin = hv.util.transform.lon_lat_to_easting_northing(*lim_mins)
-    xmax, ymax = hv.util.transform.lon_lat_to_easting_northing(*lim_maxs)
 
     my_map = (gv.tile_sources.OSM *
               satellite.opts(alpha=0.85) *
@@ -227,7 +164,7 @@ if __name__ == '__main__':
               radar_mesh_lch.opts(alpha=0.85) *
               radar_mesh_iah.opts(alpha=0.85) *
               radar_mesh_hou.opts(alpha=0.85) *
-              seg.opts(alpha=0.5, tools=['hover']) * polygon #* lower_limit * eastern_limit
+              seg.opts(alpha=0.5, tools=['hover']) * polygon * lower_limit * eastern_limit
               ).opts(width=2200, height=1200, xlim=(xmin, xmax), ylim=(ymin, ymax))
     control_column = pn.Column(date_slider, seg_sel, seg_tick, channel_select, satellite_tick, radar_sel, hgx_tick, lch_tick, iah_tick, hou_tick, write_btn)
     col = pn.Row(pn.Column(my_map), control_column)
