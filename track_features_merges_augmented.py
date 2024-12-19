2ea5459859d75cd54f17e0107327cfdb612cd5e4
Sam Gardner <sam@wx4stg.com>
2024-12-19 02:44:39 +0000

    maybe use dask in a sensible way for tfma?

--- track_features_merges_augmented.py
+++ track_features_merges_augmented.py
@@ -2,8 +2,11 @@
 
 from os import listdir, path
 from pathlib import Path
+from shutil import rmtree
 from datetime import datetime as dt, timedelta
 import sys
+
+from dask.distributed import Client
 
 import numpy as np
 import xarray as xr
@@ -12,6 +15,8 @@ from goes2go import GOES
 from glmtools.io.lightning_ellipse import lightning_ellipse_rev
 from pyxlma import coords
 from scipy.interpolate import griddata
+
+USE_DASK = True
 
 def fixup_transformed_coords(transformed_coords, input_shape):
     for transformed in transformed_coords:
@@ -52,17 +57,12 @@ def apply_coord_transforms(tfm):
     return tfm
 
 
-def add_eet_to_radar_data(tobac_day):
-    radar_path = path.join(path.sep, 'Volumes', 'LtgSSD', 'nexrad_gridded', tobac_day.strftime('%B').upper(), tobac_day.strftime('%Y%m%d'))
-    print('Finding elevations for all tobac grid points')
-    radar_path_contents = sorted(listdir(radar_path))
-    print('Starting processing...')
-
-    for this_radar_path in radar_path_contents:
+def add_eet_to_radar_data(tobac_day, client=None):
+    def compute_eet_radar(this_radar_path):
         if not this_radar_path.endswith('.nc'):
-            continue
+            return
         this_radar_path = path.join(radar_path, this_radar_path)
-        radar_data = xr.open_dataset(this_radar_path, chunks='auto')
+        radar_data = xr.open_dataset(this_radar_path)
 
         where18 = (radar_data.reflectivity >= 18)
         et_diff_from_top = 500*(where18.isel(z=slice(None, None, -1)).argmax(dim='z'))
@@ -73,32 +73,52 @@ def add_eet_to_radar_data(tobac_day):
         save_path = this_radar_path.replace('nexrad_gridded', 'nexrad_zarr').replace('.nc', '.zarr')
         Path(path.dirname(save_path)).mkdir(parents=True, exist_ok=True)
         radar_data.to_zarr(save_path)
+    radar_path = path.join(path.sep, 'Volumes', 'LtgSSD', 'nexrad_gridded', tobac_day.strftime('%B').upper(), tobac_day.strftime('%Y%m%d'))
+    print('Finding elevations for all tobac grid points')
+    radar_path_contents = sorted(listdir(radar_path))
+    print('Starting processing...')
+    all_res = []
+    for this_radar_path in radar_path_contents:
+        if client is None:
+            compute_eet_radar(this_radar_path)
+        else:
+            all_res.append(client.submit(compute_eet_radar, this_radar_path))
+
+    if client is not None:
+        client.gather(all_res)
 
 
-def add_eet_to_tobac_data(tfm, date_i_want):
-    tfm = tfm.copy()
+def add_eet_to_tobac_data(tfm, date_i_want, client=None):
+    def find_eet_feature(feature_id):
+        this_feature = tfm.sel(feature=feature_id)
+        this_feature_dt = this_feature.feature_time.data.astype('datetime64[s]').item()
+        radar_path_i_want = radar_files[np.argmin(np.abs(radar_dts - this_feature_dt))]
+        radar = xr.open_dataset(radar_path_i_want, engine='zarr').load()
+        this_eet = radar.eet_sam.isel(time=0)
+        this_seg_mask = this_feature.segmentation_mask
+        feature_eet = np.nanmax(np.where(this_seg_mask.data == feature_id, this_eet, 0))
+        return feature_eet
     radar_top_path = f'/Volumes/LtgSSD/nexrad_zarr/{date_i_want.strftime('%B').upper()}/{date_i_want.strftime('%Y%m%d')}/'
     radar_files = listdir(radar_top_path)
     radar_dts = [dt.strptime(rf, 'KHGX%Y%m%d_%H%M%S_V06_grid.zarr') for rf in radar_files if rf.endswith('.zarr')]
     radar_dts = np.array(radar_dts).astype('datetime64[s]').astype(dt)
     radar_files = [path.join(radar_top_path, rf) for rf in radar_files if rf.endswith('.zarr')]
     all_feature_eet = xr.zeros_like(tfm.feature)
-    max_itr = len(tfm.feature.data)
-    for i, feature_id in enumerate(tfm.feature.data):
-        print(f'-{100*(i/max_itr):.1f}%')
-        this_feature = tfm.sel(feature=feature_id)
-        this_feature_dt = this_feature.feature_time.data.compute().astype('datetime64[s]').item()
-        radar_path_i_want = radar_files[np.argmin(np.abs(radar_dts - this_feature_dt))]
-        radar = xr.open_dataset(radar_path_i_want, engine='zarr', chunks='auto')
-        this_eet = radar.eet_sam.isel(time=0)
-        this_seg_mask = this_feature.sel(time=this_feature_dt, method='nearest').segmentation_mask
-        feature_eet = np.nanmax(np.where(this_seg_mask.data == feature_id, this_eet, 0))
-        all_feature_eet[i] = feature_eet
+    if client is None:
+        max_iter = len(tfm.feature.data)
+        for i, feature_id in enumerate(tfm.feature.data):
+            print(f'({100*(i/max_iter):.1f}%) {i}/{max_iter}')
+            all_feature_eet[i] = find_eet_feature(feature_id)
+    else:
+        futures = client.map(find_eet_feature, tfm.feature.data)
+        all_feature_eet = client.gather(futures)
+        print(all_feature_eet)
     tfm['feature_echotop'] = all_feature_eet
     return tfm
 
 
-def find_satellite_temp_for_feature(tfm_time, feature_i_want, area_i_want):
+def find_satellite_temp_for_feature(tfm_time, feature_i_want, area_i_want, feat_echotop=0):
+    RE_HOUSTON = 6372.9*1000
     # Find the index boundaries of the feature
     x_indices_valid, y_indices_valid = np.asarray(tfm_time.segmentation_mask == feature_i_want).nonzero()
     if len(x_indices_valid) == 0:
@@ -118,7 +138,7 @@ def find_satellite_temp_for_feature(tfm_time, feature_i_want, area_i_want):
     tpcs = coords.TangentPlaneCartesianSystem(ctrLat=tfm_time.center_lat,
                                               ctrLon=tfm_time.center_lon, ctrAlt=0)
     satsys = coords.GeostationaryFixedGridSystem(subsat_lon=area_i_want.nominal_satellite_subpoint_lon.data.item(),
-                                                 sweep_axis='x')
+                                                 sweep_axis='x', ellipse=[RE_HOUSTON+feat_echotop, RE_HOUSTON+feat_echotop])
 
     # Get the ECEF coordinates of the grid centers of the rectangle containing the feature
     this_feature_ECEF = tpcs.toECEF(this_feature_x2d.flatten(), this_feature_y2d.flatten(), np.zeros_like(this_feature_y2d).flatten())
@@ -156,25 +176,18 @@ def find_satellite_temp_for_feature(tfm_time, feature_i_want, area_i_want):
         vals_i_want = np.array([np.nan])
     if np.all(np.isnan(vals_i_want)):
         min_sat_temp = np.nan
-        mean_sat_temp = np.nan
-        std_sat_temp = np.nan
     else:
         min_sat_temp = np.nanmin(vals_i_want)
-        mean_sat_temp = np.nanmean(vals_i_want)
-        std_sat_temp = np.nanstd(vals_i_want)
-    return min_sat_temp, mean_sat_temp, std_sat_temp
+    return min_sat_temp
 
 
 def add_goes_data_to_tobac_path(tfm):
-    thing_to_plot =  'L2-MCMIPC'
     tfm = tfm.copy()
     min_sat_temp = np.full(tfm.feature.shape[0], np.nan)
-    mean_sat_temp = np.full(tfm.feature.shape[0], np.nan)
-    std_satellite_temp = np.full(tfm.feature.shape[0], np.nan)
 
     goes_time_range_start = tfm.time.data.astype('datetime64[s]').astype(dt)[0]
     goes_time_range_end = tfm.time.data.astype('datetime64[s]').astype(dt)[-1]
-    goes_ctt = GOES(satellite=16, product=f'ABI-{thing_to_plot}')
+    goes_ctt = GOES(satellite=16, product='ABI-L2-MCMIPC')
     print('Start download')
     download_results = goes_ctt.timerange(goes_time_range_start-timedelta(minutes=15), goes_time_range_end+timedelta(minutes=15))
     print('End download')
@@ -209,7 +222,7 @@ def add_goes_data_to_tobac_path(tfm):
     goes_yslice = slice(goes_max_y+padding, goes_min_y-padding)
     for i, feat_id in enumerate(tfm.feature.data):
         this_feat = tfm.sel(feature=feat_id)
-        this_feature_time_idx = this_feat.feature_time_index.data.compute().item()
+        this_feature_time_idx = this_feat.feature_time_index.data.item()
         tfm_time = this_feat.isel(time=this_feature_time_idx)
         this_feat_time_dt = tfm_time.time.data.astype("datetime64[s]").astype(dt).item()
         print(this_feat_time_dt)
@@ -220,36 +233,36 @@ def add_goes_data_to_tobac_path(tfm):
             continue
         goes_file_path = download_results[download_results['tobac_idx'] == this_feature_time_idx]['file'].values[0]
         goes_file_path = path.join('/Volumes/LtgSSD/', goes_file_path)
-        satellite_data = xr.open_dataset(goes_file_path, chunks='auto').load()
+        satellite_data = xr.open_dataset(goes_file_path)
         area_i_want = satellite_data.sel(y=goes_yslice, x=goes_xsclice)
-        this_min_sat_temp, this_mean_sat_temp, this_std_sat_temp = find_satellite_temp_for_feature(tfm_time, feat_id, area_i_want)
+        this_min_sat_temp = find_satellite_temp_for_feature(tfm_time, feat_id, area_i_want, feat_echotop=this_feat.feature_echotop.data.item())
         min_sat_temp[i] = this_min_sat_temp
-        mean_sat_temp[i] = this_mean_sat_temp
-        std_satellite_temp[i] = this_std_sat_temp
 
-    tfm[f'min_{thing_to_plot}'] = xr.DataArray(min_sat_temp, dims=('feature'))
-    tfm[f'avg_{thing_to_plot}'] = xr.DataArray(mean_sat_temp, dims=('feature'))
-    tfm[f'std_{thing_to_plot}'] = xr.DataArray(std_satellite_temp, dims=('feature'))
+    tfm[f'min_L2-MCMIPC'] = xr.DataArray(min_sat_temp, dims=('feature'))
     return tfm
 
 
 if __name__ == '__main__':
+    if USE_DASK:
+        client = Client('tcp://127.0.0.1:8786')
+    else:
+        client = None
     date_i_want = sys.argv[1]
     date_i_want = dt.strptime(date_i_want, '%Y-%m-%d')
     print('Reading')
     path_to_read = f'/Volumes/LtgSSD/tobac_saves/tobac_Save_{date_i_want.strftime('%Y%m%d')}/Track_features_merges.nc'
-    tobac_data = xr.open_dataset(path_to_read, chunks='auto')
+    tobac_data = xr.open_dataset(path_to_read)
     print('Applying coord transforms')
     tobac_data = apply_coord_transforms(tobac_data)
-    tobac_data.chunk('auto').to_zarr(path_to_read.replace('.nc', '_coordinated.zarr'))
+    coords_path = path_to_read.replace('.nc', '_coords.nc')
+    if path.exists(coords_path):
+        rmtree(coords_path)
+    tobac_data.chunk('auto').to_zarr(coords_path)
     if not path.exists(f'/Volumes/LtgSSD/nexrad_zarr/{date_i_want.strftime('%B').upper()}/{date_i_want.strftime('%Y%m%d')}'):
         print('I don\'t have EET for this day, computing it')
-        add_eet_to_radar_data(date_i_want)
+        add_eet_to_radar_data(date_i_want, client)
     print('Finding echo top heights')
-    tobac_data = add_eet_to_tobac_data(tobac_data, date_i_want)
-    tobac_data.chunk('auto').to_zarr(path_to_read.replace('Track_features_merges.nc', 'tfma_checkpoint.zarr'))
-    print('Adding timeseries data to tobac data')
-    tobac_data = add_timeseries_data_to_toabc_path(tobac_data, date_i_want)
+    tobac_data = add_eet_to_tobac_data(tobac_data, date_i_want, client)
     print('Adding satellite data to tobac data')
     tobac_data = add_goes_data_to_tobac_path(tobac_data)
     print('Saving')
